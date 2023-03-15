10d30124231200c3d13677b8d1c657c3a0efe9f6
ArthurZhou <yitian.zhou@icloud.com>
2023-03-15 20:29:39 +0800

    0.0.2-s20230315

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # v2Lib ChangeLog
 ***
-Version: `0.0.2-s20230313`
+Version: `0.0.2-s20230315`
 
 Changes:
 
-1. Allow external config
+1. Add auth system
--- main.v
+++ main.v
@@ -6,16 +6,17 @@ import regex
 import json
 import log
 import time
+import net.http
 
-struct App {
+struct App { // main app context
 	vweb.Context
 }
 
-struct MDData {
+struct MDData { // markdown extended data structure
 	title string
 }
 
-struct Config {
+struct Config { // config structure
 	host string
 	port int
 	home string
@@ -24,15 +25,24 @@ struct Config {
 	log_to_file bool
 	log_level string
 }
+struct AccountData {
+	keys []struct {
+		name string
+		hash string
+	}
+}
 
-__global (
-	ver = "0.0.2-s20220313"
-	l log.Log
-	root = os.getwd()
+__global ( // global variables(add param -enable-globals when running or building)
+	ver = "0.0.2-s20220315" // version
+	l log.Log // logger
+	root = os.getwd() // root path
+	keys AccountData
+	accounts []string
 )
 
 fn read_config() {
-	if !os.exists("$root/config.json") {
+	l.debug("Reading config")
+	if !os.exists("$root/config.json") { // if config.json does not exist
 		os.create("$root/config.json") or {
 			l.error(err.str())
 		}
@@ -49,40 +59,79 @@ fn read_config() {
 	os.setenv("STORAGE", cfg_data.storage, true)
 	os.setenv("ASSETS", cfg_data.assets, true)
 
-	if cfg_data.log_to_file {
-		if !os.exists("$root/logs") {
+	if cfg_data.log_to_file { // if log to file is enabled
+		if !os.exists("$root/logs") { // if logs folder does not exist
 			os.mkdir("$root/logs", os.MkdirParams{mode: 0o777}) or {
 				l.fatal(err.str())
 			}
 		}
-		os.create("$root/logs/${time.now().str().replace(" ", "=").replace(":", "-")}.log") or {
+		log_name := time.now().str().replace(" ", "=").replace(":", "-") + ".log"
+		// log files look like: YYYY-MM-DD=HH-MM-SS.log
+		os.create("$root/logs/$log_name") or {
 			l.fatal(err.str())
 		}
-		l.set_full_logpath("$root/logs/${time.now().str().replace(" ", "=").replace(":", "-")}.log")
+		l.set_full_logpath("$root/logs/$log_name")
 		l.log_to_console_too()
+		l.warn("Logging to file: $log_name")
 	}
+	// set log levels: FATAL ERROR WARN INFO DEBUG DISABLED
+	l.info("Using log level ${cfg_data.log_level}")
 	l.set_level(log.level_from_tag(cfg_data.log_level) or {
 		l.warn("Invalid log level: ${cfg_data.log_level}")
-		log.Level.debug
+		log.Level.info
 	})
+	l.debug("Config loaded")
+}
+
+fn load_keys() {
+	l.debug("Loading keys")
+	keys = json.decode(AccountData, os.read_file("$root/keys.json") or {
+		l.fatal(err.str())
+	}) or {
+		l.fatal("Error loading keys  reason: ${err.str()}")
+	}
+	for name in keys.keys {
+		accounts << name.name
+	}
+	l.debug("Keys loaded")
+}
+
+fn load_template() {
+	l.debug("Loading templates")
+	template := os.read_file("$root/${os.getenv("ASSETS")}/index.html") or {
+		l.fatal(err.str())
+	}
+	os.setenv("TEMPLATE", template, true)
+	login := os.read_file("$root/${os.getenv("ASSETS")}/login.html") or {
+		l.fatal(err.str())
+	}
+	os.setenv("LOGIN", login, true)
+	l.debug("Templates loaded")
 }
 
 fn main() {
 	println('                     
-     ___ __    _ _   
- _ _|_  |  |  |_| |_ 
-| | |  _|  |__| | . |
- \\_/|___|_____|_|___|
+      ___ __    _ _   
+  _ _|_  |  |  |_| |_ 
+ | | |  _|  |__| | . |
+  \\_/|___|_____|_|___|
+ ---------------------
+   fast small simple
                      ')
-	l.set_level(.debug)
+	l.set_level(.debug) // set default logging level
 	l.info("Starting v2Lib  version:$ver")
-	os.setenv("ROOT", os.getwd(), false)
 	read_config()
+	load_template()
+	load_keys()
+	l.info("Starting v2Lib server on http://${os.getenv("HOST")}:${os.getenv("PORT")}/")
 	vweb.run_at(&App{}, vweb.RunParams{
 		host: os.getenv("HOST")
 		port: os.getenv("PORT").int()
 		family: .ip
-	}) or { l.fatal(err.str()) }
+		startup_message: false
+	}) or {
+		l.error(err.str())
+	}
 }
 
 fn getpath(abs_path string) string {
@@ -90,39 +139,93 @@ fn getpath(abs_path string) string {
 }
 
 ["/"]
-fn (mut app App) index() vweb.Result {
+fn (mut app App) index() vweb.Result { // redirect to home page when visiting /
 	return app.redirect("/docs/${os.getenv("HOME")}")
 }
 
+["/:path"; get]
+fn (mut app App) other(_ string) vweb.Result { // response 404 when visiting invalid path
+	return app.not_found()
+}
+
+["/account/login"; get]
+fn (mut app App) login() vweb.Result { // show login page
+	return app.html(os.getenv("LOGIN").replace("{{ ERR }}", ""))
+}
+
+["/login"; post]
+fn (mut app App) handle_login() vweb.Result { // handle login form
+	form := app.form.clone()
+	account_index := accounts.index(form["name"])
+	if account_index >= 0 {
+		current_acc := keys.keys[account_index]
+		if form["pass"] == current_acc.hash {
+			app.set_cookie(http.Cookie{
+				name: "account",
+				value: current_acc.name,
+				path: "/",
+			})
+			return app.redirect("/docs/${os.getenv("HOME")}")
+		} else {
+			return app.html(os.getenv("LOGIN").replace("{{ ERR }}", "Wrong username or password"))
+		}
+	} else {
+		return app.html(os.getenv("LOGIN").replace("{{ ERR }}", "Wrong username or password"))
+	}
+}
+
+["/logout"; post]
+fn (mut app App) handle_logout() vweb.Result { // handle login form
+	app.set_cookie(http.Cookie{
+		name: "account",
+		value: "",
+		path: "/",
+	})
+	return app.redirect("/account/login")
+}
+
 ["/assets/:path"]
-fn (mut app App) assets(path string) vweb.Result {
+fn (mut app App) assets(path string) vweb.Result { // serve assets files
 	return app.file("$root/${os.getenv("ASSETS")}/$path")
 }
 
 ["/docs/:path"]
-fn (mut app App) doc(path string) vweb.Result {
-	mut page_title := path
-	if path[utf8.len(path)-3..utf8.len(path)] == ".md" {
-		mut data := os.read_file(getpath(path)) or {
-			l.error(err.str())
-			return app.text("Cannot open file: $path")
-		}
-		if data.contains("<d2lib>") && data.contains("</d2lib>") {
-			re := regex.regex_opt("<d2lib>(.*)</d2lib>") or { l.fatal(err.str()) }
-			start, end := re.match_string(data)
-			page_data := json.decode(MDData, data[start+7..end-8]) or { l.fatal(err.str()) }
-			if page_data.title != "" {
-				page_title = page_data.title
-			}
-			data = data.replace(data[start+7..end-8], "").replace("<d2lib>", "").replace("</d2lib>", "")
-		}
-		template := os.read_file("$root/${os.getenv("ASSETS")}/index.html") or {
-			l.error(err.str())
-			return app.text("Cannot load assets")
-		}
-		return app.html(template.replace("{{ CONTENT }}", markdown.to_html(data)).replace("{{ TITLE }}", page_title))
+fn (mut app App) docs(path string) vweb.Result { // serve docs
+	if accounts.index(app.get_cookie("account") or {
+		return app.redirect("/account/login")
+	}) >= 0 {
+		mut page_title := path
+		if os.exists(getpath(path)) {
+			if path[utf8.len(path)-3..utf8.len(path)] == ".md" {
+				mut data := os.read_file(getpath(path)) or {
+					l.error(err.str())
+					return app.text("Cannot open file: $path")
+				}
+				if data.contains("<d2lib>") && data.contains("</d2lib>") {
+					re := regex.regex_opt("<d2lib>(.*)</d2lib>") or {
+						l.error("Error loading extended data for `$path` reason: ${err.str()}")
+						return app.server_error(500)
+					}
+					start, end := re.match_string(data)
+					page_data := json.decode(MDData, data[start+7..end-8]) or {
+						l.error("Error decoding extended data for `$path` reason: ${err.str()}")
+						return app.server_error(500)
+					}
+					if page_data.title != "" {
+						page_title = page_data.title
+					}
+					data = data.replace(data[start+7..end-8], "").replace("<d2lib>", "").replace("</d2lib>", "")
+				}
 
+				return app.html(os.getenv("TEMPLATE").replace("{{ CONTENT }}", markdown.to_html(data)).replace("{{ TITLE }}", page_title))
+
+			} else {
+				return app.file(getpath(path))
+			}
+		} else {
+			return app.not_found()
+		}
 	} else {
-		return app.file(getpath(path))
+		return app.redirect("/account/login")
 	}
 }
